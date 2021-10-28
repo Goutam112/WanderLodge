ca15c24817a95dbf28a367c3912f30da8333f970
Goutam112 <goutams112345@gmail.com>
2021-10-28 00:06:20 -0400

    Adding YelpCamp code to repo

---
+++ models/campground.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Review = require('./review')
+
+const ImageSchema = new mongoose.Schema({
+    url: String,
+    filename: String
+})
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_300');
+})
+
+const opts = { toJSON: { virtuals: true } };
+
+const CampgroundSchema = new mongoose.Schema({
+    title: String,
+    images: [ImageSchema],
+    geometry: {
+        type: {
+            type: String,
+            enum: ["Point"],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    },
+    price: Number,
+    description: String,
+    location: String,
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    reviews: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Review'
+    }]
+}, opts);
+
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `<strong><a style="text-decoration:none;color:black;" href="/campgrounds/${this._id}">${this.title}</a></strong>
+    <p style="text-align:center;"><a style="text-decoration:none;" href="/campgrounds/${this._id}">More info...</a></p>`
+})
+
+CampgroundSchema.post('findOneAndDelete', async function (campground) {
+    if (campground) await Review.deleteMany({ _id: { $in: campground.reviews } })
+})
+
+module.exports = mongoose.model('Campground', CampgroundSchema)
---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,19 @@
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/outdoors-v11',
+    center: campground.geometry.coordinates,
+    zoom: 10
+});
+map.addControl(new mapboxgl.NavigationControl());
+map.scrollZoom.disable();
+
+const marker = new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 35 })
+            .setHTML(
+                `<h6>${campground.title}</h6><p>${campground.location}</p>`
+            )
+    )
+    .addTo(map)
